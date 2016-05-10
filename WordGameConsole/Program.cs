8446f8a78339047fcb86916e92ea231cb0442e8f
David Diehr <ddiehr@yahoo.com>
2016-05-10 11:44:45 -0700

    Initial Commit

---
+++ WordGameConsole/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp.Authenticators;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using unirest_net.http;
+
+namespace WordGameConsole
+{
+    public class Message
+    {
+        public string word { get; set; }
+        public string also { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HttpResponse<string> response = Unirest.get("https://wordsapiv1.p.mashape.com/words/bump/also")
+            .header("X-Mashape-Key", "mpHCxg8hs7mshfJveiHzeH13kpxip1TTyWnjsnXJtVI651pQH1")
+            .header("Accept", "application/json")
+            .asJson<string>();
+            //JObject jsonResponse = (JObject)JsonConvert.DeserializeObject(response.Body);
+            //Console.WriteLine(jsonResponse["messages"]);
+            //Console.WriteLine(response.Body);
+            //Console.ReadLine();
+
+            // var client = new RestClient("https://wordsapiv1.p.mashape.com/words/");
+            // var request = new RestRequest("example/definitions?accessToke=[]", Method.GET);
+            // //4
+            // //client.Authenticator = new HttpBasicAuthenticator("X-Mashape-Key", "mgq84hQUXDmshMjka6ulmfpg3z6cp1JEg79jsnVix0eFEdnpKd");
+            // //5
+            // var response = client.Execute(request);
+            // JObject jsonResponse = (JObject)JsonConvert.DeserializeObject(response.Content);
+            Console.WriteLine("response: " + response.Body);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WordGameConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WordGameConsole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WordGameConsole")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d1220c39-351b-4f0f-ba1b-5645ffafc090")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
