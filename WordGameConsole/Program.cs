f5af1864590fb837a1c29821f1b3bbb808b8dffd
David Diehr <ddiehr@yahoo.com>
2016-05-10 15:23:44 -0700

    Add smaller wordlist.

--- WordGameConsole/Program.cs
+++ WordGameConsole/Program.cs
@@ -8,48 +8,77 @@ using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using System.Diagnostics;
 using unirest_net.http;
+using System.IO;
 
 namespace WordGameConsole
 {
-    public class Message
+    public class Result
+    {
+        public string definition { get; set; }
+        public string[] synonyms { get; set; }
+    }
+
+    public class Response
     {
         public string word { get; set; }
-        public string[] also { get; set; }
+        public Result[] results { get; set; }
     }
     class Program
     {
+        public static string[] Dictionary = File.ReadAllText("C:\\Users\\epicodus_student\\Desktop\\WordGameConsole\\WordGameConsole\\wordList.txt").Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
         static void Main(string[] args)
         {
 
-            HttpResponse<string> response = Unirest.get("https://wordsapiv1.p.mashape.com/words/bump/also")
+            Random random = new Random();
+            int randNumber = random.Next(0, Dictionary.Length);
+            var Keyword = Dictionary[randNumber];
+            HttpResponse<string> response = Unirest.get("https://wordsapiv1.p.mashape.com/words/" + Keyword + "/")
             .header("X-Mashape-Key", "mpHCxg8hs7mshfJveiHzeH13kpxip1TTyWnjsnXJtVI651pQH1")
             .header("Accept", "application/json")
             .asJson<string>();
-            JObject jsonResponse = (JObject)JsonConvert.DeserializeObject(response.Body);
 
-
-            Message newMessage = jsonResponse.ToObject<Message>();
-            
-            
-            
-            //Console.WriteLine(jsonResponse["messages"]);
-            //Console.WriteLine(response.Body);
-            //Console.ReadLine();
-
-            // var client = new RestClient("https://wordsapiv1.p.mashape.com/words/");
-            // var request = new RestRequest("example/definitions?accessToke=[]", Method.GET);
-            // //4
-            // //client.Authenticator = new HttpBasicAuthenticator("X-Mashape-Key", "mgq84hQUXDmshMjka6ulmfpg3z6cp1JEg79jsnVix0eFEdnpKd");
-            // //5
-            // var response = client.Execute(request);
-            // JObject jsonResponse = (JObject)JsonConvert.DeserializeObject(response.Content);
-            Console.WriteLine(newMessage.word);
-            foreach(var item in newMessage.also)
+            try
             {
-                Console.WriteLine(item);
+                JObject jsonResponse = (JObject)JsonConvert.DeserializeObject(response.Body);
+                Response newResponse = jsonResponse.ToObject<Response>();
+
+                if (newResponse.results != null)
+                {
+                    foreach (var item in newResponse.results)
+                    {
+
+                        Console.WriteLine("Definition: " + item.definition);
+                        if (item.synonyms != null)
+                        {
+                            Console.WriteLine("Synonyms: ");
+                            foreach (var synonym in item.synonyms)
+                            {
+
+                                Console.WriteLine(synonym);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No synonyms for " + Keyword);
+                        }
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No results for " + Keyword);
+                }
+
+                Console.WriteLine("Keyword: " + Keyword);
+                Console.ReadLine();
+
             }
-            
-            Console.ReadLine();
+            catch (Exception)
+            {
+                Console.WriteLine(Keyword + ": returns no value");
+                Console.ReadLine();
+            }
         }
     }
 }
